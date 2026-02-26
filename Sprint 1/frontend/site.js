61d863bfbecf07ce476004d2bd2f38f914355567
emileghattas <emile.ghattas05@gmail.com>
2026-02-26 15:05:41 -0500

    Overhauled entire UI for a more professional and complete look.

--- Sprint 1/frontend/site.js
+++ Sprint 1/frontend/site.js
@@ -1,11 +1,14 @@
 const authMenu = document.getElementById("authMenu");
 const userEmail = localStorage.getItem("userEmail");
+const isHome = window.location.pathname === "/";
 
 if (authMenu && userEmail) {
     authMenu.innerHTML = `
+        <a href="/" class="nav-link ${isHome ? "is-active" : ""}">Home</a>
         <div class="dropdown">
             <a class="dropdown__toggle" href="/profile">Profile</a>
             <div class="dropdown__menu">
+                <a href="/profile">Account</a>
                 <a href="#" id="signOutLink">Sign Out</a>
             </div>
         </div>
