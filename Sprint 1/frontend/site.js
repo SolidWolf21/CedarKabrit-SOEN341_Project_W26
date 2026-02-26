4ccc642aa793820cf6b28323fe67695076dbb20d
emileghattas <emile.ghattas05@gmail.com>
2026-02-26 15:10:54 -0500

    Fixed home page behaviour when user is signed in or signed out to display the correct buttons.

--- Sprint 1/frontend/site.js
+++ Sprint 1/frontend/site.js
@@ -1,6 +1,18 @@
 const authMenu = document.getElementById("authMenu");
 const userEmail = localStorage.getItem("userEmail");
 const isHome = window.location.pathname === "/";
+const guestHeroActions = document.getElementById("guestHeroActions");
+const authHeroActions = document.getElementById("authHeroActions");
+
+if (guestHeroActions && authHeroActions) {
+    if (userEmail) {
+        guestHeroActions.hidden = true;
+        authHeroActions.hidden = false;
+    } else {
+        guestHeroActions.hidden = false;
+        authHeroActions.hidden = true;
+    }
+}
 
 if (authMenu && userEmail) {
     authMenu.innerHTML = `
