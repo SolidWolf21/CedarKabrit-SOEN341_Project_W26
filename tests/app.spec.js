b18858736d03f3a7bf328a48222614178989aad8
Elfried Jeffrey Kamdem Sindjoun <alexturner0000007@gmail.com>
2026-03-28 18:59:21 -0400

    Add test for weekly planner page loading

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,3 +4,8 @@ test('MealMajor homepage loads', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page).toHaveTitle(/MealMajor/i);
 });
+
+test('weekly planner page loads', async ({ page }) => {
+  await page.goto('http://localhost:3000/planner/weekly');
+  await expect(page.locator('body')).toContainText(/weekly|planner|meal/i);
+});
